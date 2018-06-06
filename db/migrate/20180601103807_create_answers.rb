65920218f3d9f070d0e7a6810b98a0c8a87c801a
Egor Tkachenko <=>
2018-06-06 18:14:20 +0300

    Finish with module4. TDD Question and Answer controllers also tests

--- db/migrate/20180601103807_create_answers.rb
+++ db/migrate/20180601103807_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
-      t.string :title
       t.text :body
+      t.bigint :question_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2018_06_01_103807) do
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.string "title"
     t.text "body"
+    t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
